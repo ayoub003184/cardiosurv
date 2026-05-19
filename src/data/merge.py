cf5fd03637e00455286a7bf8b31bd4f118c44860
mohmaedbougcha <mohamedbougacha13@gmail.com>
2026-05-19 14:15:15 +0800

    Update merge.py

--- src/data/merge.py
+++ src/data/merge.py
@@ -5,6 +5,7 @@ df1 = pd.read_csv("src/data/data/raw/heart.csv")
 df2 = pd.read_csv("src/data/data/raw/processed.cleveland_labeled.csv")
 df3 = pd.read_csv("src/data/data/raw/processed.hungarian_labeled.csv")
 df3 = pd.read_csv("src/data/data/raw/processed.switzerland_labeled.csv")
+df4 = pd.read_csv("src/data/data/raw/processed.switzerland_labeled.csv")
 
 # Merge on common column
 merged = pd.merge(df1, df2, df3, on="id", how="inner")
